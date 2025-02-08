dae53a53bc32ff21b9eb2ddf9acbec22d5911c70
TBT-team <thebesttechforall@gmail.com>
2025-02-08 14:09:32 +0100

    Add files via upload

---
+++ aprint.py
@@ -0,0 +1,218 @@
+print("     LETTER PRINTER")
+print("TBT team. By Kaka development. All rights reserved.")
+print("KSL - Kaka Standard Letter.")
+print()
+print()
+def a():
+    print("     /|")
+    print("    / |")
+    print("   /--|")
+    print("  /   |")
+    print(" /    |")
+    print()
+def b():
+    print(" |---)")
+    print(" |   |")
+    print(" |---)")
+    print(" |   |")
+    print(" |---)")
+    print()
+def c():
+    print(" _____")
+    print("|     |")
+    print("|     ")
+    print("|     |")
+    print(" ˘˘˘˘˘")
+    print()
+def d():
+    print(" |--_")
+    print(" |   -")
+    print(" |   |")
+    print(" |   -")
+    print(" |--^")
+    print()
+def e():
+    print(" |^^^^")
+    print(" |")
+    print(" |^^^^")
+    print(" |")
+    print(" |____")
+    print()
+def f():
+    print(" |^^^^")
+    print(" |")
+    print(" |^^^^")
+    print(" |")
+    print(" |")
+    print()
+def g():
+    print(" /^^^^|")
+    print(" |    |")
+    print("  ----")
+    print("      |")
+    print("  ####")
+    print()
+def h():
+    print(" |   |")
+    print(" |   |")
+    print(" |---|")
+    print(" |   |")
+    print(" |   |")
+    print()
+def i():
+    print("   |")
+    print("   |")
+    print("   |")
+    print("   |")
+    print("   |")
+    print()
+def j():
+    print("   |")
+    print("   |")
+    print("   |")
+    print("   /")
+    print("  ˘")
+    print()
+def y():
+    print('  \    /')
+    print('   \  / ')
+    print('    /  ')
+    print('    |   ')
+    print('    |   ')
+    print()
+def k():
+    print(" |   /")
+    print(" |  /")
+    print(" |-")
+    print(" | )")
+    print(" |  )")
+    print()
+def l():
+    print(" |")
+    print(" |")
+    print(" |")
+    print(" |")
+    print(" |___")
+    print()
+def m():
+    print("  /|  /|")
+    print(" / | / |")
+    print("|  |-  |")
+    print("|  |   |")
+    print()
+def n():
+    print("    /|    /")
+    print("   / |   /")
+    print("  /  |  /")
+    print(" /   | /")
+    print("/    |/")
+    print()
+def o():
+    print(" _____")
+    print("|     |")
+    print("|     |")
+    print("|     |")
+    print(" ˘˘˘˘˘")
+    print()
+def p():
+    print(" |^^^^")
+    print(" |    |")
+    print(" |^^^^")
+    print(" |")
+    print(" |")
+    print()
+def r():
+    print(" |^^^^")
+    print(" |    |")
+    print(" |^^^^")
+    print(" | )")
+    print(" |   )")
+    print()
+def s():
+    print(" ____")
+    print(" |")
+    print(" (__")
+    print("    )")
+    print(" ___|")
+    print()
+def t():
+    print(" _____")
+    print("   |")
+    print("   |")
+    print("   |")
+    print("   |")
+    print()
+def u():
+    print(" |   |")
+    print(" |   |")
+    print(" |   |")
+    print(" |   |")
+    print("  ˘˘˘")
+    print()
+def v():
+    print(" |    /")
+    print(" |   /")
+    print(" |  /")
+    print(" | /")
+    print(" |/")
+    print()
+def z():
+    print(" _____")
+    print("     /")
+    print("    /")
+    print("   /")
+    print("  ˘˘˘˘")
+
+while True:
+    print("What KSL letter do you want?")
+    leta = input("   _>")
+    print()
+    let = leta.upper()
+    if let == "A":
+        a()
+    if let == "B":
+        b()
+    if let == "C":
+        c()
+    if let == "D":
+        d()
+    if let == "E":
+        e()
+    if let == "F":
+        f()
+    if let == "G":
+        g()
+    if let == "H":
+        h()
+    if let == "y":
+        y()
+    if let == "I":
+        i()
+    if let == "J":
+        j()
+    if let == "K":
+        k()
+    if let == "L":
+        l()
+    if let == "M":
+        m()
+    if let == "J":
+        j()
+    if let == "O":
+        o()
+    if let == "P":
+        p()
+    if let == "R":
+        r()
+    if let == "S":
+        s()
+    if let == "T":
+        t()
+    if let == "U":
+        u()
+    if let == "V":
+        v()
+    if let == "Z":
+        z()
+
+    
